13fd7d1153442741dd28f9c9d39f208792f33369
github_username <jacobrugano@gmail.com>
2022-03-28 20:27:17 +0300

    Added code to attach a photo id to the category name

--- photos/views.py
+++ photos/views.py
@@ -6,15 +6,16 @@ def gallery(request):
     categories = Category.objects.all() #To query the Database for categories from the Category model
     photos = Photo.objects.all()  #To query the Database for photos from the Photo model
     
-    context = {'categories':categories, 'photos':photos}
-    return render(request, 'photos/gallery.html', context)
+    # context = {'categories':categories, 'photos':photos}
+    return render(request, 'photos/gallery.html', {'categories':categories, 'photos':photos})
                                     # 'photos/gallery.html'...the path whwre we will output
                                     # context.....a variable holding the data we will output
+                                    # Note: The curly braces cover the entire data to be outputted
 
 def viewPhoto(request, pk):
     photo = Photo.objects.get(id=pk) # get...bcos we are not capturing everything
                                      # id=pk...we capture the photo by the id which is the primary key
-    return render(request, 'photos/photo.html')
+    return render(request, 'photos/photo.html', {'photo':photo})
 
 def addPhoto(request):
     return render(request, 'photos/add.html')
