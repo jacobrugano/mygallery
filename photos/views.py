620e8fbb28662de6f56d1a99ea8af299443ca862
github_username <jacobrugano@gmail.com>
2022-03-28 20:01:11 +0300

    Created code to display photos

--- photos/views.py
+++ photos/views.py
@@ -3,8 +3,11 @@ from .models import Category, Photo
 
 # Create your views here.
 def gallery(request):
-    categories = Category.objects.all() #To query the Database
-    return render(request, 'photos/gallery.html', {'categories':categories})
+    categories = Category.objects.all() #To query the Database for categories from the Category model
+    photos = Photo.objects.all()  #To query the Database for photos from the Photo model
+    
+    context = {'categories':categories, 'photos':photos}
+    return render(request, 'photos/gallery.html', context)
 
 def viewPhoto(request, pk):
     return render(request, 'photos/photo.html')
