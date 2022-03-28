b86e0c6c6415e9626650f03dd1a843c274b18221
github_username <jacobrugano@gmail.com>
2022-03-28 16:43:28 +0300

    Created admin

--- photos/admin.py
+++ photos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+
+from .models import Photo, Category
+
+admin.site.register(Category)
+admin.site.register(Photo)
