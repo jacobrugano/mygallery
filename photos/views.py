3726570a393609d2e5c270b687a43178fa505847
github_username <jacobrugano@gmail.com>
2022-03-28 20:13:24 +0300

    Attaching each photo to a primary key

--- photos/views.py
+++ photos/views.py
@@ -8,8 +8,12 @@ def gallery(request):
     
     context = {'categories':categories, 'photos':photos}
     return render(request, 'photos/gallery.html', context)
+                                    # 'photos/gallery.html'...the path whwre we will output
+                                    # context.....a variable holding the data we will output
 
 def viewPhoto(request, pk):
+    photo = Photo.objects.get(id=pk) # get...bcos we are not capturing everything
+                                     # id=pk...we capture the photo by the id which is the primary key
     return render(request, 'photos/photo.html')
 
 def addPhoto(request):
